b1c42a04074797ae102b3b871e385db27aeae3ca
Dyn0402 <Dyn0402@hotmail.com>
2019-07-09 20:23:55 -0700

    Fiexed some naming and elimated unused mu7.

--- Fluctuation_Lib/ratio_methods.cpp
+++ Fluctuation_Lib/ratio_methods.cpp
@@ -68,7 +68,7 @@ vector<double> rotate_angles(vector<double> angles, double rotate) {
 //Bad implementation, should use generating function or standard library which does.
 tuple<double, double> get_cumulant(vector<double> data, int n) {
 	double cumulant, err;
-	double mu1, mu2, mu3, mu4, mu5, mu6, mu7, mu8;
+	double mu1, mu2, mu3, mu4, mu5, mu6, mu8;
 	if(n == 1) {
 		mu1 = get_raw_moment(data, 1);
 		mu2 = get_raw_moment(data, 2);
@@ -95,7 +95,7 @@ tuple<double, double> get_cumulant(vector<double> data, int n) {
 		mu4 = get_raw_moment(data, 4);
 		mu5 = get_raw_moment(data, 5);
 		mu6 = get_raw_moment(data, 6);
-		mu4 = get_raw_moment(data, 8);
+		mu8 = get_raw_moment(data, 8);
 		cumulant = mu4 - 3 * pow(mu2,2);
 		err = ( mu8 - 12*mu6*mu2 - 8*mu5*mu3 - pow(mu4, 2) + 48*mu4*pow(mu2,2) + 64*pow(mu3,2)*mu2 - 36*pow(mu2,4) ) / (double)data.size();
 		err = pow(err/(double)data.size(), 0.5);
