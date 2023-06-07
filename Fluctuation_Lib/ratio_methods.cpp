0bcf77057bb63761c6c3d950718e8c5ed4e8c29d
Dyn0402 <40580528+Dyn0402@users.noreply.github.com>
2023-06-06 19:57:20 -0700

    Fixed resize insted of reserve bug in get_resamples_4 no mutex

--- Fluctuation_Lib/ratio_methods.cpp
+++ Fluctuation_Lib/ratio_methods.cpp
@@ -286,7 +286,7 @@ vector<int> get_resamples4(vector<double> angles, double bin_width, int samples,
 	unsigned num_angles = angles.size();
 	vector<int> hist(num_angles + 1, 0);
 	if (samples == 0) { return hist; }
-	angles.resize(num_angles * 2 + 1);
+	angles.reserve(num_angles * 2 + 1);
 	for (unsigned i = 0; i < num_angles; i++) {  // Duplicate angles past 2pi since bin_high will have to wrap around
 		//		if (angles[i] >= 2 * M_PI || angles[i] < 0) { cout << "get_resamples angle out of range " << angles[i] << endl; }
 		//		if (angles[i + 1] < angles[i]) { cout << "get_resamples angles unsorted! " << angles[i] << " > " << angles[i + 1] << endl; }
